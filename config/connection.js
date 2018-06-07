7f4b2391dab089e6dc740b288858eb84e0604c71
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-06-07 14:34:18 -0700

    Update connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,20 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+var msql = require('mysql');
+var connection;
+var JAWSDB_URL = "mysql://vh9xlpgqxqyjsghe:oh69d45ebk8tj8xn@j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gumbfre28v4xnuad";
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
+if ( process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burger_db"
+      });
+}
 // Export connection for our ORM to use.
 module.exports = connection;
